26b67ebd42c8122ac61442ead455aa4c4d85f89b
Alex Muñoz <alexuriel.777@gmail.com>
2021-11-25 01:12:33 -0500

    desing home

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SliderHomeComponent } from './shared/slider-home/slider-home.component';
+import { SliderFrasesHomeComponent } from './shared/slider-frases-home/slider-frases-home.component';
+
+
+
+@NgModule({
+  declarations: [HomeComponent, HeaderComponent, SliderHomeComponent, SliderFrasesHomeComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomeComponent
+  ]
+})
+export class ComponentsModule { }
