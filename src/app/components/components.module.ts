deacffc87ca23e4ac18d6d3d90f9902695b2ed9f
Alex Muñoz <alexuriel.777@gmail.com>
2021-11-30 02:05:22 -0500

    login

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './PAGES/login/login.component';
     HeaderComponent,
     SliderHomeComponent,
     SliderFrasesHomeComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule
