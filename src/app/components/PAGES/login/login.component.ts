b60c39768b92b955c0a107da8493d6a7b9e0d025
Alex Muñoz <alexuriel.777@gmail.com>
2021-12-02 01:33:47 -0500

    send email

--- src/app/components/PAGES/login/login.component.ts
+++ src/app/components/PAGES/login/login.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public mostrarOcultarRecordar: boolean = false;
+
+  constructor() {
+    this.inicializarVariables();
+  }
 
   ngOnInit(): void {
+  }
+
+  private inicializarVariables(){
+    this.mostrarOcultarRecordar = false;
+  }
+
+  recordarClave(cambiar: boolean){
+    this.mostrarOcultarRecordar = cambiar;
   }
 
 }
